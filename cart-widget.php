39a10a38db2f71713c9385e5aac83996007d6d24
Andres Canon <andres.canon@usegale.cloud>
2024-05-20 16:21:52 -0500

    first commit

---
+++ cart-widget.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: Cart Widget Coordinadora
+ * Description: Un plugin para un carrito de compras utilizando Ionic y JavaScript.
+ * Version: 1.0
+ * Author: Andrés Cañón
+ */
